89b8119c24ae4e2205451d984847bee32301d0da
Roger Tosin <roger.tosin@gmail.com>
2023-02-06 18:40:33 -0300

    refactor: Refactor initial projeto by detekt

--- app/src/main/java/com/tosin/genericproductlist/MainActivity.kt
+++ app/src/main/java/com/tosin/genericproductlist/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.tosin.genericproductlist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
