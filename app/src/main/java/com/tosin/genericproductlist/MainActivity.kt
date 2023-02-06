d6713a8a9b8d0b3f5513f706647dde94d33d7fa4
Roger Tosin <roger.tosin@gmail.com>
2023-02-06 18:33:40 -0300

    Create Android project

---
+++ app/src/main/java/com/tosin/genericproductlist/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tosin.genericproductlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
